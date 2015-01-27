0a4a24fb980d6793e8864b040e5908696289aa63
gingyneil <neil.reading.nr@gmail.com>
2015-01-27 14:23:18 -0500

    ADDED Villian class

--- COMP123Lesson4Part2/Program.cs
+++ COMP123Lesson4Part2/Program.cs
@@ -13,6 +13,9 @@ namespace COMP123Lesson4Part2
             Hero myHero = new Hero(7, 8, "female");
             myHero.doesGoodThings();
 
+          Villain  myVillian = new Villain("SpiderGirl", 9, 9, "female");
+          myVillian.doesBadThings();
+
             //WAIT FOR INPUT BEFORE CLOSING CONSOLE
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
---
+++ COMP123Lesson4Part2/Villain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123Lesson4Part2
+{
+    class Villain : Hero
+    {
+        //PRIVATE PROPERTIES +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private string name;
+        public Villain(string name, int health, int smarts, string gender) :
+            base(health, smarts, gender)
+        {
+            this.name = name;
+        
+        
+        }
+   //PUBLIC METHODS+++++++++++++++++++++++++++++++++++++++++++++++++++
+        public void doesBadThings() 
+        {
+            Console.WriteLine("Doin' bad things...........YEAH!");
+        }
+    }
+}
